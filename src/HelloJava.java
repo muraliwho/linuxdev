aaaa69744a18703ae55885eda4734c305a1c65a0
murali <muraliwho@gmail.com>
2018-07-30 19:00:05 -0400

    This is java initial commit from Eclipse IDE on Ubuntu

---
+++ src/HelloJava.java
@@ -0,0 +1,15 @@
+
+public class HelloJava {
+
+	public HelloJava() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println ("Hello Java !");
+
+	}
+
+}
